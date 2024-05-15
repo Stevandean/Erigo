59ae2e3a19b4ba271b7f26d96a688963d66e1370
Alan <pratamaassidiqqialan@gmail.com>
2024-05-15 17:30:22 +0700

    upd: create DetailTransactionController

--- backend/app/Http/Controllers/DetailTransactionController.php
+++ backend/app/Http/Controllers/DetailTransactionController.php
@@ -11,5 +11,133 @@ use Illuminate\Support\Facades\Auth;
 
 class DetailTransactionController extends Controller
 {
-    //
+   /**
+     * Show all data
+     */
+    public function index(DetailTransaction $DetailTransaction)
+    {
+        return response()->json([
+            'success' => true,
+            'message' => 'Show all data',
+            'data' => $DetailTransaction::all()
+        ], 200);
+    }
+     /**
+     * Create data
+     */
+    public function store(Request $request, DetailTransaction $DetailTransaction)
+    {
+        $validator = Validator::make(
+            $request->all(),
+            [
+                'quantity' => 'required',
+                'total_price' => 'required'
+            ]
+        );
+
+        if ($validator->fails()) {
+            return Response()->json($validator->errors());
+        }
+
+        $store = $categories::create([
+            'quantity' => $request->quantity,
+            'total_price' => $request->total_price
+
+        ]);
+
+        $data = $categories::where('quantity', '=', $request->quantity)->get();
+        if ($store) {
+            return Response()->json([
+                'status' => 1,
+                'message' => 'Success create new data!',
+                'data' => $data
+            ]);
+        } else {
+            return Response()->json([
+                'status' => 0,
+                'message' => 'Failed create data!'
+            ]);
+        }
+    }
+    /**
+     * Show data by id
+     */
+    public function show(DetailTransaction $DetailTransaction, $detail_transaction_id)
+    {
+        if ($DetailTransaction::where('detail_transaction_id', $detail_transaction_id)->exists()) {
+            $data = $DetailTransaction::where('DetailTransaction.detail_transaction_id', '=', $detail_transaction_id)->get();
+
+            return response()->json([
+                'success' => true,
+                'message' => 'Success show data!',
+                'data' => $data
+            ], 200);
+        } else {
+            return response()->json([
+                'success' => false,
+                'message' => 'Failed find the data!',
+                'data' => ''
+            ], 404);
+        }
+    }
+    /**
+     * Update data
+     */
+    public function update(Request $request, DetailTransaction $DetailTransaction, $detail_transaction_id)
+    {
+        $validator = Validator::make(
+            $request->all(),
+            [
+                'quantity' => 'required',
+                'total_price' => 'required'
+            ]
+        );
+
+        if ($validator->fails()) {
+            return Response()->json($validator->errors());
+        }
+
+        $update = DB::table('detail_transaction')
+            ->where('detail_transaction_id', '=', $detail_transaction_id)
+            ->update([
+                'quantity' => $request->quantity,
+                'total_price' => $request->total_price
+            ]);
+
+        $data = $categories::where('detail_transaction_id', '=', $detail_transaction_id)->get();
+        if ($update) {
+            return Response()->json([
+                'status' => 1,
+                'message' => 'Success updating data !',
+                'data' => $data
+            ]);
+        } else {
+            return Response()->json([
+                'status' => 0,
+                'message' => 'Failed updating data !'
+            ]);
+        }
+    }
+     /**
+     * Delete data
+     */
+    public function destroy($id)
+    {
+        $delete = DB::table('detail_transaction')
+            ->where('detail_transaction_id', '=', $id)
+            ->delete();
+
+        if ($delete) {
+            return Response()->json([
+                'status' => 1,
+                'message' => 'Success delete data !'
+            ]);
+        } else {
+            return Response()->json([
+                'status' => 0,
+                'message' => 'Failed delete data !'
+            ]);
+        }
+    }
 }
+
