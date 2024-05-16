ad54ccc8155c9b6adcdba435a066de71ccd280a1
Dean <stevandeanachmad1@gmail.com>
2024-05-16 20:17:39 +0700

    upd: add metadata for register page

--- frontend/src/app/auth/register/page.tsx
+++ frontend/src/app/auth/register/page.tsx
@@ -1,27 +1,23 @@
-import { FC } from 'react';
-import type { GetServerSideProps } from 'next';
-import { getSession } from 'next-auth/react';
+import { FC } from "react";
+import { Metadata } from "next";
 
-import ContainerRegister from '@/components/Containers/Auth/Register';
+import ContainerRegister from "@/components/Containers/Auth/Register";
 
-// Fungsi dibawah ini digunakan untuk memblokir halaman Register jika user sudah ter-autentikasi
-export const getServerSideProps: GetServerSideProps = async ({ req }) => {
-  const session = await getSession({ req });
-
-  if (session) {
-    return {
-      redirect: {
-        destination: '/',
-        permanent: false,
-      },
-    };
-  }
-
-  return {
-    props: {
-      session,
-    },
-  };
+export const metadata: Metadata = {
+  title: "Register | Erigo Store",
+  description:
+    "Welcome to Erigo Store! Discover the latest in fashion and accessories at Erigo Store, your ultimate destination for trendy and affordable styles. Whether you're looking for casual wear, business attire, or something for a special occasion, Erigo Store has it all.",
+  openGraph: {
+    url: "http://localhost:3000/",
+    description:
+      "Welcome to Erigo Store! Discover the latest in fashion and accessories at Erigo Store, your ultimate destination for trendy and affordable styles. Whether you're looking for casual wear, business attire, or something for a special occasion, Erigo Store has it all.",
+    title: "Welcome to Erigo Store!",
+  },
+  twitter: {
+    title: "Welcome to Erigo Store!",
+    description:
+      "Welcome to Erigo Store! Discover the latest in fashion and accessories at Erigo Store, your ultimate destination for trendy and affordable styles. Whether you're looking for casual wear, business attire, or something for a special occasion, Erigo Store has it all.",
+  },
 };
 
 const Register: FC = () => {
