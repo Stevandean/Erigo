fc0b91a6dd7e7b6cc50cd43b633de33ceb1e8805
Ainu Azzaria <ainu.azzaria@gmail.com>
2024-05-16 20:37:06 +0700

    feat: create new controller for product and transaction

--- backend/app/Http/Controllers/ProductController.php
+++ backend/app/Http/Controllers/ProductController.php
@@ -11,5 +11,158 @@ use Illuminate\Support\Facades\Auth;
 
 class ProductController extends Controller
 {
-    //
+    /**
+     * Show all data
+     */
+    public function index(Product $product)
+    {
+        return response()->json([
+            'success' => true,
+            'message' => 'Success show all data!',
+            'data' => $product::all()
+        ], 200);
+    }
+
+    /**
+     * create data
+     */
+
+    public function store(Request $request, Product $product)
+    {
+        $validator = Validator::make(
+            $request->all(),
+            [
+                'product_name' => 'required',
+                'price' => 'required',
+                'desc' => 'required',
+                'size' => 'required',
+                'stock' => 'required',
+                'pict' => 'required',
+                'rating' => 'required',
+                'categories_id' => 'required',
+            ]
+        );
+
+        if ($validator->fails()) {
+            return Response()->json($validator->errors());
+        }
+
+        $store = $product::create([
+            'product_name' => $request->product_name,
+            'price' => $request->price,
+            'desc' => $request->desc,
+            'size' => $request->size,
+            'stock' => $request->stock,
+            'pict' => $request->pict,
+            'rating' => $request->rating,
+            'categories_id' => $request->categories_id,
+        ]);
+
+        $data = $product::where('product_name', '=', $request->product_name)->get();
+        if ($store) {
+            return Response()->json([
+                'status' => 1,
+                'message' => 'Success create new data!',
+                'data' => $data
+            ]);
+        } else {
+            return Response()->json([
+                'status' => 0,
+                'message' => 'Failed create data!'
+            ]);
+        }
+    }
+
+    /**
+     * show data by id
+     */
+
+
+    public function show(Product $product, $product_id)
+    {
+        if ($product::where('product_id', $product_id)->exists()) {
+            $data = $product::join('categories', 'categories.categories_id', '=', 'product.categories_id')->where('product_id', $product_id)->get();
+
+            return response()->json([
+                'success' => true,
+                'message' => 'Success show data!',
+                'data' => $data
+            ], 200);
+        } else {
+            return response()->json([
+                'success' => false,
+                'message' => 'Failed find the data!',
+                'data' => ''
+            ], 404);
+        }
+    }
+
+    /**
+     * Update data
+     */
+    public function update(Request $request, Product $product, $product_id)
+    {
+        $validator = Validator::make(
+            $request->all(),
+            [
+                'product_name' => 'required',
+                'price' => 'required',
+                'desc' => 'required',
+                'size' => 'required',
+                'stock' => 'required',
+                'pict' => 'required',
+                'rating' => 'required',
+                'categories_id' => 'required',
+            ]
+        );
+
+        if ($validator->fails()) {
+            return Response()->json($validator->errors());
+        }
+
+        $update = DB::table('product')->where('product_id', '=', $product_id)->update([
+            'product_name' => $request->product_name,
+            'price' => $request->price,
+            'desc' => $request->desc,
+            'size' => $request->size,
+            'stock' => $request->stock,
+            'pict' => $request->pict,
+            'rating' => $request->rating,
+            'categories_id' => $request->categories_id,
+        ]);
+
+        $data = $product::where('product_id', '=', $product_id)->get();
+        if ($update) {
+            return Response()->json([
+                'status' => 1,
+                'message' => 'Success updating data!',
+                'data' => $data
+            ]);
+        } else {
+            return Response()->json([
+                'status' => 0,
+                'message' => 'Failed updating data!'
+            ]);
+        }
+    }
+
+    /**
+     * Delete data
+     */
+    public function destroy($id)
+    {
+        $delete = DB::table('product')->where('product_id', '=', $id)->delete();
+
+        if ($delete) {
+            return Response()->json([
+                'status' => 1,
+                'message' => 'Success delete data!'
+            ]);
+        } else {
+            return Response()->json([
+                'status' => 0,
+                'message' => 'Failed delete data!'
+            ]);
+        }
+    }
 }
--- backend/app/Http/Controllers/TransactionController.php
+++ backend/app/Http/Controllers/TransactionController.php
@@ -11,5 +11,144 @@ use Illuminate\Support\Facades\Auth;
 
 class TransactionController extends Controller
 {
-    //
+    /**
+     * Show all data
+     */
+    public function index(Transaction $transaction)
+    {
+        return response()->json([
+            'success' => true,
+            'message' => 'Success show all data!',
+            'data' => $transaction::all()
+        ], 200);
+    }
+
+    /**
+     * Create data
+     */
+    public function store(Request $request, Transaction $transaction)
+    {
+        $validator = Validator::make(
+            $request->all(),
+            [
+                'receipt_number' => 'required',
+                'payment_method' => 'required',
+                'status_payment' => 'required',
+                'status_courier' => 'required',
+                'users_id' => 'required'
+
+            ]
+        );
+
+        if ($validator->fails()) {
+            return Response()->json($validator->errors());
+        }
+
+        $store = $transaction::create([
+            'receipt_number' => $request->receipt_number,
+            'payment_method' => $request->payment_method,
+            'status_payment' => $request->status_payment,
+            'status_courier' => $request->status_courier,
+            'users_id' => $request->users_id
+        ]);
+
+        $data = $transaction::where('receipt_number', '=', $request->receipt_number)->get();
+        if ($store) {
+            return Response()->json([
+                'status' => 1,
+                'message' => 'Success create new data!',
+                'data' => $data
+            ]);
+        } else {
+            return Response()->json([
+                'status' => 0,
+                'message' => 'Failed create data!'
+            ]);
+        }
+    }
+
+    /**
+     * Show data by id
+     */
+    public function show(Transaction $transaction, $transaction_id)
+    {
+        if ($transaction::where('transaction_id', $transaction_id)->exists()) {
+            $data = $transaction::where('transaction.transaction_id', '=', $transaction_id)->get();
+
+            return response()->json([
+                'success' => true,
+                'message' => 'Success show data!',
+                'data' => $data
+            ], 200);
+        } else {
+            return response()->json([
+                'success' => false,
+                'message' => 'Failed find the data!',
+                'data' => ''
+            ], 404);
+        }
+    }
+
+    /**
+     * Update data
+     */
+    public function update(Request $request, Transaction $transaction, $transaction_id)
+    {
+        $validator = Validator::make(
+            $request->all(),
+            [
+                'receipt_number' => 'required',
+                'payment_method' => 'required',
+                'status_payment' => 'required',
+                'status_courier' => 'required',
+                'users_id' => 'required'
+            ]
+        );
+
+        if ($validator->fails()) {
+            return Response()->json($validator->errors());
+        }
+
+        $update = DB::table('transaction')->where('transaction_id', '=', $transaction_id)->update([
+            'receipt_number' => $request->receipt_number,
+            'payment_method' => $request->payment_method,
+            'status_payment' => $request->status_payment,
+            'status_courier' => $request->status_courier,
+            'users_id' => $request->users_id
+        ]);
+
+        $data = $transaction::where('transaction_id', '=', $transaction_id)->get();
+        if ($update) {
+            return Response()->json([
+                'status' => 1,
+                'message' => 'Success updating data!',
+                'data' => $data
+            ]);
+        } else {
+            return Response()->json([
+                'status' => 0,
+                'message' => 'Failed updating data!'
+            ]);
+        }
+    }
+
+    /**
+     * Delete data
+     */
+    public function destroy($id)
+    {
+        $delete = DB::table('transaction')->where('transaction_id', '=', $id)->delete();
+
+        if ($delete) {
+            return Response()->json([
+                'status' => 1,
+                'message' => 'Success delete data!'
+            ]);
+        } else {
+            return Response()->json([
+                'status' => 0,
+                'message' => 'Failed delete data!'
+            ]);
+        }
+    }
 }
