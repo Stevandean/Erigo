7974dbeadb0c03d8ed5e150c18a23d2e3db65f44
Dean <stevandeanachmad1@gmail.com>
2024-05-17 20:18:11 +0700

    upd: update page payment

--- frontend/src/components/Containers/Payment/components/PaymentMethod.tsx
+++ frontend/src/components/Containers/Payment/components/PaymentMethod.tsx
@@ -3,19 +3,21 @@ import { FC } from "react";
 const PaymentMethod: FC = () => {
   return (
     <div className="flex flex-col">
-      <span className="text-xl font-semibold">Select Method</span>
-      <div className="w-36 text-lg font-medium flex justify-between">
+    <span className="text-xl font-semibold">Select Method</span>
+    <div className="w-36 text-lg font-medium flex justify-between">
         <span>QRIS</span>
         <span></span>
-      </div>
-      <div className="flex justify-end items-center space-x-10">
+    </div>
+    <div className="flex justify-end items-center space-x-3">
         <span className="font-semibold text-lg">Total : Rp.752.999</span>
         <button className="text-white bg-navy rounded-md px-3 py-2">
-          <a href="./qris.html">Payment</a>
+            <a href="./qris.html">
+                Payment
+            </a>
         </button>
-      </div>
     </div>
-  );
+</div>
+  )
 };
 
 export default PaymentMethod;
