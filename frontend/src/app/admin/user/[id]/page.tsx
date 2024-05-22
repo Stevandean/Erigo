bc4b2663b5e146054a8f30755d8908165ea82e16
Dean <stevandeanachmad1@gmail.com>
2024-05-22 16:35:07 +0700

    feat: admin feature

---
+++ frontend/src/app/admin/user/[id]/page.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+import ContainerAdminEditUser from "@/components/Containers/Admin/User/Edit/Edit";
+
+const AdminEditUser: FC = () => {
+  return <ContainerAdminEditUser />;
+};
+
+export default AdminEditUser;
---
+++ frontend/src/app/admin/user/page.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+import ContainerAdminUser from "@/components/Containers/Admin/User/User";
+
+const AdminUser: FC = () => {
+  return <ContainerAdminUser />;
+};
+
+export default AdminUser;
