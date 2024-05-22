1d3062d67f2f6552dc4d841709a88dcb5d202466
Dean <stevandeanachmad1@gmail.com>
2024-05-22 16:34:40 +0700

    upd: add new interface

--- frontend/src/interfaces/user.ts
+++ frontend/src/interfaces/user.ts
@@ -1,8 +1,10 @@
 export interface User {
+  id?: number;
   name: string;
   address: string;
   phone: string;
   email: string;
   password: string;
-  role: string,
+  pict?: string;
+  role: string;
 }
